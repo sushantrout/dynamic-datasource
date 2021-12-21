661b58af0f1bf8d360a4ffccd5a746afdaf3b5ce
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2021-12-21 14:25:14 +0530

    primary changes
    primary changes

--- dynamic-datasource-builder/src/main/java/com/bip/DynamicDatasourceBuilderApplication.java
+++ dynamic-datasource-builder/src/main/java/com/bip/DynamicDatasourceBuilderApplication.java
@@ -1,4 +1,4 @@
-package com.bip.dynamicdatasourcebuilder;
+package com.bip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
