835ba619b73e2713411deaad907d0c6bd708c646
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2021-12-21 13:58:24 +0530

    initisal commit
    initisal commit

---
+++ dynamic-datasource-builder/src/main/java/com/bip/dynamicdatasourcebuilder/DynamicDatasourceBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.bip.dynamicdatasourcebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatasourceBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicDatasourceBuilderApplication.class, args);
+	}
+
+}
---
+++ dynamic-datasource-builder/src/test/java/com/bip/dynamicdatasourcebuilder/DynamicDatasourceBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bip.dynamicdatasourcebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDatasourceBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
